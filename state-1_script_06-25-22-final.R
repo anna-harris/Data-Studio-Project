43cd39b5afa7c2389f4ea54e4300aced305ba856
Anna Harris <afisharris@gmail.com>
2022-06-27 17:32:27 -0400

    2nd script update

--- state-1_script_06-25-22-final.R
+++ state-1_script_06-25-22-final.R
@@ -12,86 +12,86 @@ library(fasttime)
 setwd("~/Dropbox/My Mac (Anna’s MacBook Pro)/Desktop/Data Studio/state_name")
 
 #read csv
-state-1_ehist_data <- read_csv("ehist_Atlantic.csv")
+state_1_ehist_data <- read_csv("ehist_Atlantic.csv")
 
 #clean_names
-state-1_ehist_data_clean_names <- clean_names(state-1_ehist_data)
+state_1_ehist_data_clean_names <- clean_names(state_1_ehist_data)
 
 #select columns
-state-1_ehist_selected <- select(state-1_ehist_data_clean_names, voter_id, voter_legacy_id, voter_status, 
+state_1_ehist_selected <- select(state_1_ehist_data_clean_names, voter_id, voter_legacy_id, voter_status, 
                             voter_registration_date, voter_first, voter_last, voter_sex, 
                            voter_dob, voter_phone, res_city, res_state, res_zipcode, 
                             election_name, election_date, election_type
                             )
 
 #check type and class of col object
-#typeof(state-1_ehist_selected$voter_registration_date)
+#typeof(state_1_ehist_selected$voter_registration_date)
 
-#class(state-1_ehist_selected$voter_registration_date)
+#class(state_1_ehist_selected$voter_registration_date)
 
 #change type
-state-1_ehist_selected[, c(9, 12)] <- lapply(state-1_ehist_selected[, c(9, 12)], as.numeric)
+state_1_ehist_selected[, c(9, 12)] <- lapply(state_1_ehist_selected[, c(9, 12)], as.numeric)
 
 #read csv
-state-1_vlist_data <- read_csv("vlist_Atlantic.csv")
+state_1_vlist_data <- read_csv("vlist_Atlantic.csv")
 
 #clean col names
-state-1_vlist_data_clean_names <- clean_names(state-1_vlist_data)
+state_1_vlist_data_clean_names <- clean_names(state_1_vlist_data)
 
 #select cols
-state-1_vlist_selected <- select(state-1_vlist_data_clean_names, display_id, reg_date, status, first, last)
+state_1_vlist_selected <- select(state_1_vlist_data_clean_names, display_id, reg_date, status, first, last)
 
 #check col object type and class
-#typeof(state-1_vlist_selected$reg_date)
+#typeof(state_1_vlist_selected$reg_date)
 
-#class(state-1_vlist_selected$reg_date)
+#class(state_1_vlist_selected$reg_date)
 
 #convert datetime to date
-system.time(state-1_vlist_selected$reg_date <- as.Date(state-1_vlist_selected$reg_date))
+system.time(state_1_vlist_selected$reg_date <- as.Date(state_1_vlist_selected$reg_date))
 
 
 #to uppercase for join
 
-state-1_vlist_upper <- state-1_vlist_selected %>% 
+state_1_vlist_upper <- state_1_vlist_selected %>% 
   mutate(across(where(is.character), toupper)) 
 
-state-1_ehist_upper <- state-1_ehist_selected %>% 
+state_1_ehist_upper <- state_1_ehist_selected %>% 
   mutate(across(where(is.character), toupper))
 
-#class(state-1_vlist_upper$reg_date)
+#class(state_1_vlist_upper$reg_date)
 
 
 #join vlist and ehist
-combined <- state-1_vlist_upper %>% full_join(state-1_ehist_upper, by = c("display_id" = "voter_id"))
+combined <- state_1_vlist_upper %>% full_join(state_1_ehist_upper, by = c("display_id" = "voter_id"))
 
 #people who appear on the voter list but have no voter history
 never_voters <- combined %>%  filter(is.na(status) == TRUE | is.na(voter_status) == TRUE)
 
 #read csv --> this data needs crime type
-state-1_corrections_data <- read_csv("OPRA #20334 - Release data.csv")
+state_1_corrections_data <- read_csv("OPRA #20334 - Release data.csv")
 
 
 #clean_names
-state-1_corrections_data_clean_names <- clean_names(state-1_corrections_data)
+state_1_corrections_data_clean_names <- clean_names(state_1_corrections_data)
 
 #to upper
 
-state-1_corrections_upper <- state-1_corrections_data_clean_names %>% 
+state_1_corrections_upper <- state_1_corrections_data_clean_names %>% 
   mutate(across(where(is.character), toupper))
 
 #convert characters to dates
 
-state-1_corrections_upper$released <- mdy(state-1_corrections_upper$released)
-state-1_corrections_upper$dob <- mdy(state-1_corrections_upper$dob)
+state_1_corrections_upper$released <- mdy(state_1_corrections_upper$released)
+state_1_corrections_upper$dob <- mdy(state_1_corrections_upper$dob)
 
 #check col object type and class
-#typeof(state-1_corrections_upper$dob)
+#typeof(state_1_corrections_upper$dob)
 
-#class(state-1_corrections_upper$dob)
+#class(state_1_corrections_upper$dob)
 
 
 
-combined_w_corrections <- state-1_corrections_upper %>% full_join(combined, by = c("last", "first", "dob" = "voter_dob"))
+combined_w_corrections <- state_1_corrections_upper %>% full_join(combined, by = c("last", "first", "dob" = "voter_dob"))
 
 typeof(combined$voter_dob)
 
@@ -99,7 +99,7 @@ test <- combined %>%  filter(display_id == "P4253156165")
 
 test2 <- combined_w_corrections %>%  filter(is.na(sbi) == TRUE)
 
-test3 <- state-1_corrections_upper %>%  filter(is.na(sbi) == TRUE)
+test3 <- state_1_corrections_upper %>%  filter(is.na(sbi) == TRUE)
 
 registered_formerly_incarcerated_individuals <- combined_w_corrections %>%  filter(!is.na(sbi) == TRUE & !is.na(display_id) == TRUE)
 
